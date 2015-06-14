3cbe6c1220720e532067e9c07f5170a2df72093e
Ameya Thakur <redsupercar@gmail.com>
2015-06-14 14:24:47 -0700

    Yo initialization

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name voluntutorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the voluntutorApp
+ */
+angular.module('voluntutorApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name voluntutorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the voluntutorApp
+ */
+angular.module('voluntutorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
